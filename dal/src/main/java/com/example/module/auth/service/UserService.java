f403ad5fef962ac28463635d8d1e09b1edc750f7
eblank <1292002150@qq.com>
2018-11-07 16:56:05 +0800

    模块中bean只能在xml中定义, 不能通过注解方式

--- dal/src/main/java/com/example/module/auth/service/UserService.java
+++ dal/src/main/java/com/example/module/auth/service/UserService.java
@@ -1,9 +1,11 @@
 package com.example.module.auth.service;
 
+import com.alipay.sofa.runtime.api.annotation.SofaService;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.example.module.auth.dao.UserDao;
 import com.example.module.auth.domain.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 /**
  * <p>
@@ -14,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
  * @since 2017-08-07
  */
 @Slf4j
+@SofaService
 public class UserService extends ServiceImpl<UserDao, User> implements IUserService {
 	@Override
 	public String getById(Long id) {
